9b57371c0d461a02530c464150058828fcbdf33f
Fritz Jay <jonathan.jay10@gmail.com>
2018-07-31 22:12:15 -0700

    TestNumbers expand on mobile

--- src/Components/TestNumber/TestNumber.tsx
+++ src/Components/TestNumber/TestNumber.tsx
@@ -1,8 +1,7 @@
 import * as React from 'react';
 import { ITestNumber } from '../../lib/Interfaces';
 import { themeColors } from '../../lib/Themes';
-import { Card } from '../Card/Card';
-import { Operator } from '../Operator/Operator';
+import { Button, Card, Operator } from '../Components';
 import './TestNumber.css';
 
 interface IProps {
@@ -11,13 +10,22 @@ interface IProps {
   onCardClick: (testNumber: ITestNumber) => void;
 }
 
-export class TestNumber extends React.Component <IProps>{
+interface IState {
+  active: boolean;
+}
+
+export class TestNumber extends React.Component <IProps, IState>{
   public constructor(props: IProps) {
     super(props);
+    this.state = {
+      active: false,
+    }
     this.handleCardClick = this.handleCardClick.bind(this);
+    this.handleStartClick = this.handleStartClick.bind(this);
   }
 
   public render() {
+    const buttonContainerClassName = this.state.active ? 'button-container active' : 'button-container';
     const operators = this.props.number.operators.map((operator, i) => {
       const color = themeColors[i % themeColors.length];
       return (
@@ -36,11 +44,22 @@ export class TestNumber extends React.Component <IProps>{
         <div className="operators-container">
             {operators}
         </div>
+        <div className={buttonContainerClassName}>
+          <Button className="button" onClick={this.handleStartClick}>Create Test</Button>
+        </div>
       </Card>
     );
   }
 
   private handleCardClick() {
-    this.props.onCardClick(this.props.number);
+    this.setState((prevState: IState) => {
+      return {active: !prevState.active}
+    }, () => {
+      // this.props.onCardClick(this.props.number);
+    });
+  }
+
+  private handleStartClick() {
+    console.log('START');
   }
 }
