16a7a59cc3c505feb88111ad3fc9f49a68fdfb5f
Fritz Jay <jonathan.jay10@gmail.com>
2018-08-03 23:30:02 -0700

    Added an error when the Create Test button is clicked when an operator isn't selected.

--- src/Components/TestNumber/TestNumber.tsx
+++ src/Components/TestNumber/TestNumber.tsx
@@ -13,20 +13,22 @@ interface IProps {
 }
 
 interface IState {
+  error: boolean;
   operator?: string;
 }
 
 export class TestNumber extends React.Component <IProps, IState>{
   public constructor(props: IProps) {
     super(props);
-    this.state = {}
+    this.state = {
+      error: false,
+    }
     this.activateCard = this.activateCard.bind(this);
     this.handleStartClick = this.handleStartClick.bind(this);
     this.handleOperatorClick = this.handleOperatorClick.bind(this);
   }
 
   public render() {
-    const className = this.props.active ? 'test-number active' : 'test-number';
     const operators = this.props.number.operators.map((operator, i) => {
       const color = themeColors[i % themeColors.length];
       const selected = (this.props.active && this.state.operator === operator);
@@ -42,7 +44,7 @@ export class TestNumber extends React.Component <IProps, IState>{
       );
     });
     return (
-      <Card onClick={this.activateCard} className={className}>
+      <Card onClick={this.activateCard} className={`test-number ${this.props.active && 'active'} ${this.state.error && 'error'}`}>
         <div className="header">
           <p className={`text ${this.props.color}`}>{this.props.number.number}</p>
         </div>
@@ -51,6 +53,7 @@ export class TestNumber extends React.Component <IProps, IState>{
         </div>
         <div className="button-container">
           <Button className="button" onClick={this.handleStartClick}>Create Test</Button>
+          <p className="error-message">Please select an operator</p>
         </div>
       </Card>
     );
@@ -59,17 +62,25 @@ export class TestNumber extends React.Component <IProps, IState>{
   private activateCard() {
     this.props.onClick(this.props.number.number);
     if (!this.props.active) {
-      this.setState({operator: undefined});
+      this.setState({
+        error: false,
+        operator: undefined,
+      });
     }
   }
 
   private handleOperatorClick(operator: string) {
-    this.setState({operator});
+    this.setState({
+      error: false,
+      operator,
+    });
   }
 
   private handleStartClick() {
     if (this.state.operator) {
       this.props.onSubmit(this.props.number, this.state.operator);
+    } else {
+      this.setState({error: true});
     }
   }
 }
