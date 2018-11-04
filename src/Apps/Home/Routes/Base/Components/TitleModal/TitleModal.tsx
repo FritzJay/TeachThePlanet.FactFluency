475b2074e818b685f33d42f6d4735a4b3fc43e4b
unknown <thomasisaacpetereccles@gmail.com>
2018-11-04 15:48:04 -0800

    Title Modal Expansion with box shadow

--- src/Apps/Home/Routes/Base/Components/TitleModal/TitleModal.tsx
+++ src/Apps/Home/Routes/Base/Components/TitleModal/TitleModal.tsx
@@ -18,14 +18,14 @@ export class TitleModal extends React.Component<IProps, any> {
     return (
       <Modal className="title-modal">
         <ModalHeader> 
-          <p className="title-header">Practice your math facts here.</p>
+          <p className="title-header">Practice Your Math Facts Here</p>
         </ModalHeader>
 
         <ModalContent>
         <Link className="title-btn" to={`${this.props.match.url}login`}>
           <Button className="green">Get Started</Button>
         </Link>
-        <p className="title-sub">Anytime. Anywhere. Always Free.</p>
+        <p className="title-sub">Anytime. Almost Anywhere. Always Free.</p>
         </ModalContent>
       </Modal>
     );
