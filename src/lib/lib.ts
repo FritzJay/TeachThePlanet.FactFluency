14879c7296f87c2914e5888dd3298709d168e2e2
Fritz Jay <jonathan.jay10@gmail.com>
2018-11-09 22:50:26 -0800

    fixed exports

--- src/lib/lib.ts
+++ src/lib/lib.ts
@@ -1,11 +1,5 @@
-import * as _Caching from './Caching';
-import * as _Requests from './Requests';
-import * as _Testing from './Testing/Testing';
-import * as _Themes from './Themes';
-import * as _Utility from './Utility/Utility';
-
-export const Caching = _Caching;
-export const Requests = _Requests;
-export const Testing = _Testing;
-export const Themes = _Themes;
-export const Utility = _Utility;
+export * from './Caching';
+export * from './Requests';
+export * from './Testing/Testing';
+export * from './Themes';
+export * from './Utility/Utility';
