614f05e422cd6aa63fca7e7078475397f89dc9d2
Fritz Jay <jonathan.jay10@gmail.com>
2018-11-11 11:37:00 -0800

    Added redux dev tools to the app

--- src/redux/middleware/index.ts
+++ src/redux/middleware/index.ts
@@ -1,5 +1,5 @@
 import { applyMiddleware } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
 import thunk from 'redux-thunk'
-import logger from './logger'
 
-export default applyMiddleware(thunk, logger)
+export default composeWithDevTools(applyMiddleware(thunk))
