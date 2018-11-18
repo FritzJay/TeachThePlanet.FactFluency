59d69f983e69bb0abf3490b574f4fc6e216c97f6
Fritz Jay <jonathan.jay10@gmail.com>
2018-11-18 00:52:08 -0800

    Updated updateTestParameters query

--- src/utils/api/testParameters.ts
+++ src/utils/api/testParameters.ts
@@ -1,58 +1,17 @@
 import { ITestParameters, IUpdateTestParameters } from ".."
 import { handleError } from "./request"
 
-
-export const fetchTestParameters = async (token: string, testParametersID: string): Promise<ITestParameters> => {
-  const functionName = 'fetchTestParameters'
-  const query = `
-    query getTestParameters($token: String!, $id: String!) {
-      getTestParameters(token: $token, id: $id) {
-        id
-        duration
-        numbers
-        operators
-        questions
-        randomQuestions
-      }
-    }
-  `
-  try {
-    const response = await fetch(`${process.env.REACT_APP_API_URL}/graphql`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json',
-      },
-      body: JSON.stringify({
-        query,
-        variables: { token, id: testParametersID }
-      })
-    })
-    const { data, errors } = await response.json()
-
-    if (errors !== undefined) {
-      throw errors[0]
-    }
-
-    return data.getTestParameters
-
-  } catch (error) {
-    handleError(functionName, error)
-    throw error
-  }
-}
-
-export const saveUpdateTestParameters = async (token: string, testParametersID: string, updates: IUpdateTestParameters): Promise<ITestParameters> => {
+export const saveUpdateTestParameters = async (token: string, testParametersID: string, input: IUpdateTestParameters): Promise<ITestParameters> => {
   const functionName = 'saveUpdateTestParameters'
   const query = `
-    mutation changeTestParameters($token: String!, $updates: TestParametersInput!) {
-      changeTestParameters(token: $token, updates: $updates) {
+    mutation updateTestParameters($id: ObjID!, $input: UpdateTestParametersInput!) {
+      updateTestParameters(id: $id, input: $input) {
         id
         duration
-        numbers
-        operators
         questions
         randomQuestions
+        numbers
+        operators
       }
     }
   `
@@ -62,10 +21,12 @@ export const saveUpdateTestParameters = async (token: string, testParametersID: 
       headers: {
         'Content-Type': 'application/json',
         'Accept': 'application/json',
+        'Authorization': 'jwt ' + token,
       },
       body: JSON.stringify({
         query,
-        variables: { token, updates: { id: testParametersID, ...updates }}
+        variables: { id: testParametersID, input },
+        operationName: 'updateTestParameters',
       })
     })
     const { data, errors } = await response.json()
@@ -74,7 +35,7 @@ export const saveUpdateTestParameters = async (token: string, testParametersID: 
       throw errors[0]
     }
 
-    return data.changeTestParameters
+    return data.updateTestParameters
 
   } catch (error) {
     handleError(functionName, error)
