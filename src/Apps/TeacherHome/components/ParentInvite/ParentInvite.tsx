8717345211652d213a7c3537eef8b6f80b253865
unknown <thomasisaacpetereccles@gmail.com>
2018-11-18 14:37:56 -0800

    Parent Invite Html Can't see in browser

--- src/Apps/TeacherHome/components/ParentInvite/ParentInvite.tsx
+++ src/Apps/TeacherHome/components/ParentInvite/ParentInvite.tsx
@@ -1,5 +1,45 @@
 import * as React from 'react'
 
 export const ParentInvite = () => {
-  return <div />
+  return <div>
+      <h2>Teach The Planet: Fact Fluency</h2>
+      
+      <h4>Dear Parents,</h4>
+      <p>This year we are focusing on fact fluency as part of our math curriculum. While we will be using the program during school hours, students are encouraged to practice at home.</p>
+
+      <h4>Help your child by registering a free parent account now: </h4>
+      <p>
+        <ol>
+          <li>Go to <a href="www.ttp-factfluency.herokuapp.com">www.ttp-factfluency.herokuapp.com</a></li>
+          <li>Enter your account details to create your parent account.</li>
+          <li>Once logged in, follow the on-boarding instructions.</li>
+          <li>Add your child by entering your students class code and student code.</li>
+        </ol>
+      </p>
+
+      <h4>Real Time Reports</h4>
+      <p>A Teach The Planet account lets your student practice their fact fluency in accordance with the classroom needs and goals and reports their progress in real time.</p>
+
+      <h4>How does my child practice at home?</h4>
+      <p>
+        <ol>
+          <li>Go to <a href="www.ttp-factfluency.herokuapp.com">www.ttp-factfluency.herokuapp.com</a></li>
+          <li>Select the student user type.</li>
+          <li>Enter the students email or username listed below.</li>
+          <li>Enter the password listed below.</li>
+          <li>Select a number and operator (×,÷,−,+) and get started!</li>
+        </ol>
+      </p>
+
+      <table>
+        <tr>
+          <th>Email/Username</th>
+          <th>Password</th>
+        </tr>
+        <tr>
+          <th>dynamic email</th>
+          <th>dynamic password</th>
+        </tr>
+      </table>
+    </div>
 }
