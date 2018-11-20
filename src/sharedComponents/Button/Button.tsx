27a82b750802bdcd33cf16cf0ff34dc44bab96cf
Fritz Jay <jonathan.jay10@gmail.com>
2018-11-19 20:49:20 -0800

    added a confirm button component

--- src/sharedComponents/Button/Button.tsx
+++ src/sharedComponents/Button/Button.tsx
@@ -12,3 +12,36 @@ export const Button = (props: any) => {
     </button>
   );
 }
+
+interface IState {
+  confirm: boolean
+}
+
+export class ConfirmButton extends React.Component<any, IState> {
+  public state = {
+    confirm: false
+  }
+
+  public render() {
+    const { className, children, confirmClassName } = this.props
+
+    return (
+      <Button
+        {...this.props}
+        className={`${className ? className : ''}${confirmClassName ? ' ' + confirmClassName : ''}`}
+        onClick={this.handleClick}
+      >
+        {children
+          ? children
+          : null
+        }
+      </Button>
+    )
+  }
+
+  private handleClick = (e: any) => {
+    this.setState({ confirm: true },
+      () => this.props.onClick && this.props.onClick(e)
+    )
+  }
+}
