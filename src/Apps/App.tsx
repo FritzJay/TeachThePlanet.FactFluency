8edc64d3ca4b584ec936a5a6fc6395aaf68a3ebf
Fritz Jay <jonathan.jay10@gmail.com>
2018-11-27 18:36:26 -0800

    Redirect to login on /

--- src/Apps/App.tsx
+++ src/Apps/App.tsx
@@ -2,6 +2,7 @@ import * as React from 'react'
 import {
   Route,
   Switch,
+  Redirect,
 } from 'react-router-dom'
 
 import LoadingBar from 'react-redux-loading-bar'
@@ -19,6 +20,12 @@ export default class App extends React.Component<any> {
         <LoadingBar className="LoadingBar" />
         
         <Switch>
+          <Route
+            path="/"
+            exact={true}
+            render={() => <Redirect to="/index" />}
+          />
+
           <Route
             path="/index"
             component={Login}
