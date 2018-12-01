d8d79584fa6a1fd3ebc3538becacb124f19b7cb6
Fritz Jay <jonathan.jay10@gmail.com>
2018-11-30 20:37:27 -0800

    Moved the apollo provider to inside the error boundary to properly catch errors.

--- src/Apps/App.tsx
+++ src/Apps/App.tsx
@@ -4,6 +4,9 @@ import {
   Switch,
   Redirect,
 } from 'react-router-dom'
+import { ApolloProvider } from 'react-apollo'
+import ApolloClient, { InMemoryCache, Operation } from 'apollo-boost'
+import { factFluencyDefaults, factFluencyResolvers } from './FactFluency/clientState'
 
 import Login from 'src/Apps/Login/Login'
 import { PageNotFound } from 'src/sharedComponents'
@@ -12,38 +15,58 @@ import { FactFluency } from './FactFluency/FactFluency'
 import { ErrorBoundary } from 'src/sharedComponents/ErrorBoundary/ErrorBoundary'
 import './App.css'
 
+const client = new ApolloClient({
+  uri: 'http://localhost:3000/graphql',
+  cache: new InMemoryCache(),
+  request: async (operation: Operation) => {
+    const token = localStorage.getItem('token')
+    operation.setContext({ headers: {
+      authorization: token ? `jwt ${token}` : ''
+    }})
+  },
+  clientState: {
+    defaults: {
+      ...factFluencyDefaults(),
+    },
+    resolvers: {
+      ...factFluencyResolvers(),
+    }
+  },
+})
 
 export default class App extends React.Component<any> {
   public render() {
     return (
-      <div className="App">
-        <ErrorBoundary>
-          <Switch>
-            <Route
-              path="/"
-              exact={true}
-              render={() => <Redirect to="/index" />}
-            />
+      <ApolloProvider client={client}>
+        <div className="App">
+          <ErrorBoundary>
+            <Switch>
+              <Route
+                path="/"
+                exact={true}
+                render={() => <Redirect to="/index" />}
+              />
 
-            <Route
-              path="/index"
-              component={Login}
-            />
+              <Route
+                path="/index"
+                component={Login}
+              />
 
-            <Route
-              path="/fact-fluency"
-              component={FactFluency}
-            />
+              <Route
+                path="/fact-fluency"
+                component={FactFluency}
+              />
 
-            <Route
-              path="/teacher"
-              component={TeacherHome}
-            />
+              <Route
+                path="/teacher"
+                component={TeacherHome}
+              />
 
-            <Route component={PageNotFound} />
-          </Switch>
-        </ErrorBoundary>
-      </div>
+              <Route component={PageNotFound} />
+            </Switch>
+          </ErrorBoundary>
+        </div>
+      </ApolloProvider>
     )
   }
 }
--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,10 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 
-import { ApolloProvider } from 'react-apollo'
-import ApolloClient, { InMemoryCache, Operation } from 'apollo-boost'
 import { BrowserRouter } from 'react-router-dom'
 import * as WebFont from 'webfontloader'
 
 import App from './Apps/App'
-import { factFluencyDefaults, factFluencyResolvers } from './Apps/FactFluency/clientState'
 
 WebFont.load({
   google: {
@@ -20,30 +17,9 @@ WebFont.load({
   },
 })
 
-const client = new ApolloClient({
-  uri: 'http://localhost:3000/graphql',
-  cache: new InMemoryCache(),
-  request: async (operation: Operation) => {
-    const token = localStorage.getItem('token')
-    operation.setContext({ headers: {
-      authorization: token ? `jwt ${token}` : ''
-    }})
-  },
-  clientState: {
-    defaults: {
-      ...factFluencyDefaults(),
-    },
-    resolvers: {
-      ...factFluencyResolvers(),
-    }
-  },
-})
-
 ReactDOM.render((
-  <ApolloProvider client={client}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </ApolloProvider>),
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>),
   document.getElementById('root') as HTMLElement
 )
